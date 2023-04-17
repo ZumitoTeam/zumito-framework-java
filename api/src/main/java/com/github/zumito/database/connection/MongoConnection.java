3497a612b7b05932ad4163565785b2a9d1a93cae
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-04-17 22:21:06 +0200

    feat -> added mongo connection and interface

---
+++ api/src/main/java/com/github/zumito/database/connection/MongoConnection.java
@@ -0,0 +1,31 @@
+package com.github.zumito.database.connection;
+
+import com.github.zumito.database.impl.IConnection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoConnection implements IConnection {
+    private MongoClientURI mongoClientURI;
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+
+    @Override
+    public void start() {
+        String uri = "mongodb+srv://joselufupa2016:RWGBIbQ6BnJYhMWQ@cluster0.qqfu6ud.mongodb.net/test";
+        String database = "db_test";
+
+        mongoClientURI = new MongoClientURI(uri);
+        mongoClient = new MongoClient(mongoClientURI);
+        mongoDatabase = mongoClient.getDatabase(database);
+    }
+
+    @Override
+    public void stop() {
+        mongoClient.close();
+    }
+
+    public MongoDatabase getMongoDatabase(){
+        return mongoDatabase;
+    }
+}
---
+++ api/src/main/java/com/github/zumito/database/impl/IConnection.java
@@ -0,0 +1,6 @@
+package com.github.zumito.database.impl;
+
+public interface IConnection  {
+    void start();
+    void stop();
+}
