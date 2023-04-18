9b88aab2b9a1bbf07f83737078cda5b92cd6b428
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-04-18 15:35:13 +0200

    feat -> added file to main class to load config.yml

--- api/src/main/java/com/github/zumito/RunApp.java
+++ api/src/main/java/com/github/zumito/RunApp.java
@@ -1,21 +1,33 @@
 package com.github.zumito;
 
+import com.github.zumito.file.FileManager;
 import net.dv8tion.jda.api.JDABuilder;
+import org.yaml.snakeyaml.Yaml;
+import sun.security.krb5.Config;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class RunApp {
     private ZumitoAPI zumitoAPI;
+    private Yaml yaml;
+    private FileManager fileManager;
 
     public void run() {
+
         try {
+            fileManager = new FileManager("config.yml");
             zumitoAPI.jda = JDABuilder
-                    .createDefault("TOKENHERE")
+                    .createDefault(fileManager.getString("token"))
                     .build()
                     .awaitReady();
-            //Jda.addEventListener(new MessageHandler());
-
+            yaml = new Yaml();
+            Config config = yaml.loadAs(new FileInputStream("config.yml"), Config.class);
         } catch (InterruptedException e) {
             e.printStackTrace();
             System.exit(1);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("[ZumitoJava] Started Zumito Bot!");
     }
