66c9ae05fd79b3d7c4ca79bb63dc63441159e34e
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-05-03 10:02:48 +0200

    refactor -> database functions

--- api/src/main/java/com/github/zumito/database/manager/MongoManager.java
+++
@@ -1,4 +0,0 @@
-package com.github.zumito.database.manager;
-
-public class MongoManager {
-}
---
+++ api/src/main/java/com/github/zumito/database/model/Database.java
@@ -0,0 +1,8 @@
+package com.github.zumito.database.model;
+
+public enum Database {
+    MONGO,
+    MYSQL,
+    POSTGRESQL,
+    MARIADB;
+}
