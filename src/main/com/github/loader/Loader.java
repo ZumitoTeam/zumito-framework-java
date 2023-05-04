a5b36e8c31b9b40e64b835d700080b7dbd2a8af9
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-05-04 15:36:36 +0200

    .

--- src/main/com/github/loader/Loader.java
+++ src/main/com/github/loader/Loader.java
@@ -6,21 +6,24 @@ import java.util.Scanner;
 
 public class Loader {
     public static void main (String[] args){
-        String token;
-        String databaseName;
-        Scanner scanner = new Scanner(System.in);
+        new LoaderHandler().downloadFrameWork();
 
-        System.out.print("Put your bot token here: ");
-        token = scanner.next();
 
-        System.out.print("Select db (MYSQL/MONGODB/MARIADB/POSTGRESS): ");
-        databaseName = scanner.next();
-        Database.valueOf(databaseName);
-
-        System.out.print("Bot token: "
-                + token
-                + "\n"
-                + "Database: "
-                + databaseName);
+//        String token;
+//        String databaseName;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Put your bot token here: ");
+//        token = scanner.next();
+//
+//        System.out.print("Select db (MYSQL/MONGODB/MARIADB/POSTGRESS): ");
+//        databaseName = scanner.next();
+//        Database.valueOf(databaseName);
+//
+//        System.out.print("Bot token: "
+//                + token
+//                + "\n"
+//                + "Database: "
+//                + databaseName);
     }
 }
