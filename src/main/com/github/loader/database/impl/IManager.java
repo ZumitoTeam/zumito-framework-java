47f23139f700570ba08ca03a0d10106206999ca4
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-05-04 14:44:49 +0200

    feat -> create Dabatase manager interface

--- src/main/com/github/loader/database/impl/IManager.java
+++ src/main/com/github/loader/database/impl/IManager.java
@@ -1,5 +1,14 @@
 package main.com.github.loader.database.impl;
 
+import java.util.List;
+
 public interface IManager {
+
+    String getString();
+    int getInt();
+    double getDouble();
+    boolean getBoolean();
+    List getStringList();
+    float getFloat();
 
 }
