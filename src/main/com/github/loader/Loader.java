c692a0d9165f9b02b8f7025f90a041c5f7829929
Joseluu <77939750+Joselu315@users.noreply.github.com>
2023-05-04 14:38:09 +0200

    feat -> create a system to select db and token bot

---
+++ src/main/com/github/loader/Loader.java
@@ -0,0 +1,22 @@
+package main.com.github.loader;
+
+import main.com.github.loader.database.model.Database;
+
+import java.util.Scanner;
+
+public class Loader {
+    public static void main (String[] args){
+        String token;
+        String databaseName;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Put your bot token here: ");
+        token = scanner.next();
+
+        System.out.print("Select db (MYSQL/MONGODB/MARIADB/POSTGRESS): ");
+        databaseName = scanner.next();
+        Database.valueOf(databaseName);
+
+        System.out.print("Creating file...");
+    }
+}
--- src/main/com/github/loader/database/model/Database.java
+++ src/main/com/github/loader/database/model/Database.java
@@ -1,8 +1,18 @@
 package main.com.github.loader.database.model;
 
 public enum Database {
-    MONGO,
-    MYSQL,
-    POSTGRESQL,
-    MARIADB;
+    MONGO("MONGO"),
+    MYSQL("MYSQL"),
+    POSTGRESQL("POSTGRESQL"),
+    MARIADB("MARIADB");
+
+    private final String name;
+
+    Database(String name) {
+        this.name = name;
+    }
+
+    public String getDatabaseName() {
+        return name;
+    }
 }
